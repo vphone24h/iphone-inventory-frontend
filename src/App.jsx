a435cb362ae16a6b64b7ca332d387faa1600064e
Lê Vũ <vphone24h@192.168.1.231.non-exists.ptr.local>
2025-05-21 17:42:26 +0700

    ✅ Rename login.jsx → Login.jsx để fix lỗi Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"; // ✅ sửa lại đúng thư mục
+import Login from "./pages/Login";
 
 function App() {
   return (
