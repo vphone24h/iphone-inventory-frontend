130941a573a0bdf8ba2ddbc74e2f0a0971fb1e12
Lê Vũ <vphone24h@192.168.1.231.non-exists.ptr.local>
2025-05-21 18:23:29 +0700

    🚀 Finalize Auth with PrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import DangKy from "./DangKy";
-import QuenMatKhau from "./QuenMatKhau";
-import NhapHang from "./NhapHang";
-import XuatHang from "./XuatHang";
-import TonKhoSoLuong from "./TonKhoSoLuong";
-import BaoCao from "./BaoCao";
-import PrivateRoute from "../components/PrivateRoute"; // ✅ import
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <Routes>
-      {/* Các route không cần đăng nhập */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/dang-ky" element={<DangKy />} />
-      <Route path="/quen-mat-khau" element={<QuenMatKhau />} />
-
-      {/* ✅ Các route cần đăng nhập */}
-      <Route
-        path="/nhap-hang"
-        element={
-          <PrivateRoute>
-            <NhapHang />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/xuat-hang"
-        element={
-          <PrivateRoute>
-            <XuatHang />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/ton-kho"
-        element={
-          <PrivateRoute>
-            <TonKhoSoLuong />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/bao-cao"
-        element={
-          <PrivateRoute>
-            <BaoCao />
-          </PrivateRoute>
-        }
-      />
-
-      {/* Redirect root về login nếu không có path cụ thể */}
-      <Route path="*" element={<Login />} />
-    </Routes>
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
