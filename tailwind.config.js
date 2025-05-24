f949248d4d6df54600f260385e69ac1dfc93f6d9
Lê Vũ <vphone24h@192.168.1.231.non-exists.ptr.local>
2025-05-24 22:55:26 +0700

    ♻️ Đổi tên NhapHang.jsx để ép Vercel rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -8,4 +8,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+};
